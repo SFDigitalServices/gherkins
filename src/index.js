b2bdb0a251b68efe135828432ea421f2d899b6a3
Shawn Allen <shawnbot@users.noreply.github.com>
2020-12-30 09:28:28 -0800

    add CUCUMBER_DEFAULT_TIMEOUT support, default to 10 seconds

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 require('./types')
 require('./steps')
 
+const {
+  CUCUMBER_DEFAULT_TIMEOUT = 10000
+} = process.env
+
 const World = require('./world')
 const cucumber = require('@cucumber/cucumber')
 
 cucumber.setWorldConstructor(World)
+
+if (CUCUMBER_DEFAULT_TIMEOUT) {
+  cucumber.setDefaultTimeout(CUCUMBER_DEFAULT_TIMEOUT)
+}
 
 cucumber.BeforeAll(function () {
 })
