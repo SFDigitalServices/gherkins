25ae12ba74b8c83c3637169e229e27214417988f
Shawn Allen <shawnbot@users.noreply.github.com>
2020-12-29 21:54:38 -0800

    nix console.warn() calls in hooks

--- src/index.js
+++ src/index.js
@@ -7,19 +7,15 @@ const cucumber = require('@cucumber/cucumber')
 cucumber.setWorldConstructor(World)
 
 cucumber.BeforeAll(function () {
-  console.warn('---- BeforeAll ----')
 })
 
 cucumber.Before(function () {
-  console.warn('---- Before ----')
 })
 
 cucumber.After(function () {
-  console.warn('---- After ----')
   // return this.visit('about:blank')
 })
 
 cucumber.AfterAll(function () {
-  console.warn('---- AfterAll ----')
   return World.closeAll()
 })
